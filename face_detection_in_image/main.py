eb4a467c157ffac10e7d6edf1b7ec933181e36ca
Dulon <80118217+Dulon18@users.noreply.github.com>
2021-06-08 23:19:03 +0600

    Create main.py

---
+++ face_detection_in_image/main.py
@@ -0,0 +1,18 @@
+# pip install opencv-python
+
+import cv2
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img=cv2.imread('image.png')
+
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces=face_cascade.detectMultiScale(gray,1.1,4)
+
+# drow rectangle in face
+for(x,y,w,h) in faces:
+       cv2.rectangle(img,(x,y),(x+w ,y+h),(55,50,255),2)
+    
+#display image
+cv2.imshow('Face Detection',img)
+cv2.waitKey()
