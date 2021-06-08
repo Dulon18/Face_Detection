8e051e854c0a7c563456a8cfe4d06495b3b867ab
Dulon <80118217+Dulon18@users.noreply.github.com>
2021-06-09 00:24:56 +0600

    Update main.py

--- face_detection_in_image/main.py
+++ face_detection_in_image/main.py
@@ -6,10 +6,10 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img=cv2.imread('image.png')
 
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
+#Detect faces
 faces=face_cascade.detectMultiScale(gray,1.1,4)
 
-# drow rectangle in face
+# draw rectangle in face
 for(x,y,w,h) in faces:
        cv2.rectangle(img,(x,y),(x+w ,y+h),(55,50,255),2)
     
